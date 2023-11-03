d36853f8de0a970156e1d1870a2a479a8dc6fe78
crazy0629 <kenzo.egghead@gmail.com>
2023-11-03 04:26:48 -0700

    add: chat feature

--- controllers/chat.controller.ts
+++ controllers/chat.controller.ts
@@ -1,7 +1,50 @@
 import { Response, Request } from "express";
 import Chat from "../models/Chat";
+import User from "../models/User";
 import Multer from "multer";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const socketIO = require("socket.io");
+const clients = new Set();
+
+const addConnection = (io, userId) => {
+  let flag = 0;
+  clients.forEach((client: any) => {
+    if (client.userId == userId) flag = 1;
+  });
+  if (!flag) {
+    clients.add({ io, userId });
+  }
+};
+
+const sendToReceiver = (receiverId, data) => {
+  clients.forEach((client: any) => {
+    if (client.userId == receiverId) client.io.emit("server", data);
+  });
+};
+
+export const setupWebSocket = (server) => {
+  const io = require("socket.io")(server, {
+    cors: {
+      origin: `${process.env.HOST_URL}`,
+      methods: ["GET", "POST"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+      credentials: true,
+    },
+  });
+
+  io.on("connection", (socket) => {
+    // Handle incoming messages from the client
+    addConnection(io, socket.handshake.query.userId);
+
+    // Handle client disconnection
+    socket.on("disconnect", () => {
+      console.log("A client disconnected");
+    });
+  });
+};
 
 export const addMessage = async (req: Request, res: Response) => {
   try {
@@ -27,6 +70,21 @@ export const addMessage = async (req: Request, res: Response) => {
     newChatObj.originalFileNames = chatFileOriginalNames;
 
     await newChatObj.save();
+
+    const messages = await Chat.find({
+      $or: [
+        { senderId: req.body.senderId, receiverId: req.body.receiverId },
+        { senderId: req.body.receiverId, receiverId: req.body.senderId },
+      ],
+    })
+      .populate("senderId")
+      .populate("receiverId")
+      .sort({ sentDate: 1 });
+    sendToReceiver(req.body.receiverId, messages);
+    return res.json({
+      success: true,
+      data: messages,
+    });
   } catch (error) {
     return res.json({
       success: false,
@@ -87,7 +145,10 @@ export const getMessageHistory = async (req: Request, res: Response) => {
         { senderId: req.body.senderId, receiverId: req.body.receiverId },
         { senderId: req.body.receiverId, receiverId: req.body.senderId },
       ],
-    }).sort({ sentDate: 1 });
+    })
+      .populate("senderId")
+      .populate("receiverId")
+      .sort({ sentDate: 1 });
 
     return res.json({
       success: true,
@@ -98,6 +159,64 @@ export const getMessageHistory = async (req: Request, res: Response) => {
     return res.json({
       success: false,
       message: "Error found loading messages",
+    });
+  }
+};
+
+export const addMemberOnChat = async (req: Request, res: Response) => {
+  try {
+    if (req.body.posterId != "no-user") {
+      const message = await Chat.find({
+        $or: [
+          { senderId: req.body.userId, receiverId: req.body.posterId },
+          { senderId: req.body.posterId, receiverId: req.body.userId },
+        ],
+      });
+      if (message.length == 0) {
+        const newChatItem = new Chat();
+        newChatItem.senderId = req.body.userId;
+        newChatItem.receiverId = req.body.posterId;
+        newChatItem.message = "";
+        await newChatItem.save();
+      }
+    }
+
+    const messageHistory = await Chat.find({
+      $or: [{ senderId: req.body.userId }, { receiverId: req.body.userId }],
+    });
+
+    let chatUserIdLists: any = [];
+    for (let index = 0; index < messageHistory.length; index++) {
+      if (messageHistory[index].senderId == req.body.userId)
+        chatUserIdLists.push(messageHistory[index].receiverId);
+      else chatUserIdLists.push(messageHistory[index].senderId);
+    }
+
+    const chatUserLists = await User.find({ _id: { $in: chatUserIdLists } });
+
+    let messages: any = [];
+    if (req.body.posterId != "no-user") {
+      messages = await Chat.find({
+        $or: [
+          { senderId: req.body.userId, receiverId: req.body.posterId },
+          { senderId: req.body.posterId, receiverId: req.body.userId },
+        ],
+      })
+        .populate("senderId")
+        .populate("receiverId")
+        .sort({ sentDate: 1 });
+    }
+
+    return res.json({
+      success: true,
+      data: chatUserLists,
+      messages,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: "Error found while loading messages",
     });
   }
 };
