8813f11a922a1748bd8850586feff8ae22c05c55
crazy0629 <milan.jansen0629@gmail.com>
2024-03-27 18:05:14 +0900

    finish: real estate controller

--- controllers/estate.controller.ts
+++ controllers/estate.controller.ts
@@ -152,6 +152,53 @@ export const getMoreEstateAds = async (req: Request, res: Response) => {
           req.body.bathrooms.indexOf(item.itemDetailInfo.bathrooms) !== -1
       );
     }
+    if (req.body.tenure && req.body.tenure?.length) {
+      let index = req.body.tenure.indexOf("Not Specified");
+      req.body.tenure[index] = "";
+      nextEstateAds = nextEstateAds.filter(
+        (item: any) =>
+          req.body.tenure.indexOf(item.itemDetailInfo.tenure) !== -1
+      );
+    }
+    if (req.body.condition && req.body.condition?.length) {
+      let index = req.body.condition.indexOf("Not Specified");
+      req.body.condition[index] = "";
+      nextEstateAds = nextEstateAds.filter(
+        (item: any) =>
+          req.body.condition.indexOf(item.itemDetailInfo.condition) !== -1
+      );
+    }
+    if (req.body.year && req.body.year?.length) {
+      let index = req.body.year.indexOf("Not Specified");
+      req.body.year[index] = "";
+      nextEstateAds = nextEstateAds.filter(
+        (item: any) => req.body.year.indexOf(item.itemDetailInfo.year) !== -1
+      );
+    }
+    if (req.body.energy && req.body.energy?.length) {
+      let index = req.body.energy.indexOf("Not Specified");
+      req.body.energy[index] = "";
+      nextEstateAds = nextEstateAds.filter(
+        (item: any) =>
+          req.body.energy.indexOf(item.itemDetailInfo.energy) !== -1
+      );
+    }
+    if (req.body.nearest && req.body.nearest?.length) {
+      let index = req.body.nearest.indexOf("Not Specified");
+      req.body.nearest[index] = "";
+      nextEstateAds = nextEstateAds.filter(
+        (item: any) =>
+          req.body.nearest.indexOf(item.itemDetailInfo.nearest) !== -1
+      );
+    }
+    if (req.body.facilities && req.body.facilities?.length) {
+      let index = req.body.facilities.indexOf("Not Specified");
+      req.body.facilities[index] = "";
+      nextEstateAds = nextEstateAds.filter(
+        (item: any) =>
+          req.body.facilities.indexOf(item.itemDetailInfo.facilities) !== -1
+      );
+    }
     return res.json({
       success: true,
       message: "Successfully loaded!",
@@ -220,7 +267,13 @@ export const getCountOfEachFilter = async (req: Request, res: Response) => {
             checkSellerRatingMatches(req.body.filter, obj) &&
             checkPropertyTypeMatches(req.body.filter, obj) &&
             checkBedroomsMatches(req.body.filter, obj) &&
-            checkBathroomsMatches(req.body.filter, obj)
+            checkBathroomsMatches(req.body.filter, obj) &&
+            checkTenureMatches(req.body.filter, obj) &&
+            checkPropertyConditionMatches(req.body.filter, obj) &&
+            checkYearMatches(req.body.filter, obj) &&
+            checkEnergyMatches(req.body.filter, obj) &&
+            checkNearestMatches(req.body.filter, obj) &&
+            checkFacilitiesMatches(req.body.filter, obj)
           );
         })
         .map((item: any, index: number) => {
@@ -276,6 +329,12 @@ export const getCountOfEachFilter = async (req: Request, res: Response) => {
     let itemPropertyTypeCountList = [];
     let itemBedroomsCountList = [];
     let itemBathroomsCountList = [];
+    let itemTenureCountList = [];
+    let itemPropertyConditionCountList = [];
+    let itemYearCountList = [];
+    let itemEnegryCountList = [];
+    let itemNearestCountList = [];
+    let itemFacilitiesCountList = [];
 
     if (req.body.itemSellerRating) {
       itemSellerRatingCountList = await getCountOnSellerRating(
@@ -298,6 +357,33 @@ export const getCountOfEachFilter = async (req: Request, res: Response) => {
     if (req.body.itemBathrooms) {
       itemBathroomsCountList = await getCountOnBathrooms(req.body, estateObj);
     }
+
+    if (req.body.itemTenure) {
+      itemTenureCountList = await getCountOnTenure(req.body, estateObj);
+    }
+
+    if (req.body.itemPropertyCondition) {
+      itemPropertyConditionCountList = await getCountOnPropertyCondition(
+        req.body,
+        estateObj
+      );
+    }
+
+    if (req.body.itemYear) {
+      itemYearCountList = await getCountOnYear(req.body, estateObj);
+    }
+
+    if (req.body.itemEnergy) {
+      itemEnegryCountList = await getCountOnEnergy(req.body, estateObj);
+    }
+
+    if (req.body.itemNearest) {
+      itemNearestCountList = await getCountOnNearest(req.body, estateObj);
+    }
+
+    if (req.body.itemFacilities) {
+      itemFacilitiesCountList = await getCountOnFacilities(req.body, estateObj);
+    }
     return res.json({
       success: true,
       itemPriceRange: countPerPrice,
@@ -306,6 +392,12 @@ export const getCountOfEachFilter = async (req: Request, res: Response) => {
       itemPropertyType: itemPropertyTypeCountList,
       itemBedrooms: itemBedroomsCountList,
       itemBathrooms: itemBathroomsCountList,
+      itemTenure: itemTenureCountList,
+      itemPropertyCondition: itemPropertyConditionCountList,
+      itemYear: itemYearCountList,
+      itemEnergy: itemEnegryCountList,
+      itemNearest: itemNearestCountList,
+      itemFacilities: itemFacilitiesCountList,
     });
   } catch (error) {
     console.log(error);
@@ -324,7 +416,13 @@ const getCountOnMinMaxPrice = async (mainParam, estateObj) => {
       checkSellerRatingMatches(mainParam.filter, obj) &&
       checkPropertyTypeMatches(mainParam.filter, obj) &&
       checkBedroomsMatches(mainParam.filter, obj) &&
-      checkBathroomsMatches(mainParam.filter, obj)
+      checkBathroomsMatches(mainParam.filter, obj) &&
+      checkTenureMatches(mainParam.filter, obj) &&
+      checkPropertyConditionMatches(mainParam.filter, obj) &&
+      checkYearMatches(mainParam.filter, obj) &&
+      checkEnergyMatches(mainParam.filter, obj) &&
+      checkNearestMatches(mainParam.filter, obj) &&
+      checkFacilitiesMatches(mainParam.filter, obj)
     );
   })?.length;
   return countPerPrice;
@@ -348,7 +446,13 @@ const getCountOnSellerRating = async (mainParam, estateObj) => {
         checkPriceMatches(mainParam.minPrice, mainParam.maxPrice, obj) &&
         checkPropertyTypeMatches(mainParam.filter, obj) &&
         checkBedroomsMatches(mainParam.filter, obj) &&
-        checkBathroomsMatches(mainParam.filter, obj)
+        checkBathroomsMatches(mainParam.filter, obj) &&
+        checkTenureMatches(mainParam.filter, obj) &&
+        checkPropertyConditionMatches(mainParam.filter, obj) &&
+        checkYearMatches(mainParam.filter, obj) &&
+        checkEnergyMatches(mainParam.filter, obj) &&
+        checkNearestMatches(mainParam.filter, obj) &&
+        checkFacilitiesMatches(mainParam.filter, obj)
       );
     })?.length;
 
@@ -372,7 +476,13 @@ const getCountOnPropertyType = async (mainParam, estateObj) => {
         checkPriceMatches(mainParam.minPrice, mainParam.maxPrice, obj) &&
         checkSellerRatingMatches(mainParam.filter, obj) &&
         checkBedroomsMatches(mainParam.filter, obj) &&
-        checkBathroomsMatches(mainParam.filter, obj)
+        checkBathroomsMatches(mainParam.filter, obj) &&
+        checkTenureMatches(mainParam.filter, obj) &&
+        checkPropertyConditionMatches(mainParam.filter, obj) &&
+        checkYearMatches(mainParam.filter, obj) &&
+        checkEnergyMatches(mainParam.filter, obj) &&
+        checkNearestMatches(mainParam.filter, obj) &&
+        checkFacilitiesMatches(mainParam.filter, obj)
       );
     })?.length;
     itemPropertyTypeCountList.push({
@@ -398,7 +508,13 @@ const getCountOnBedrooms = async (mainParam, estateObj) => {
         checkPriceMatches(mainParam.minPrice, mainParam.maxPrice, obj) &&
         checkSellerRatingMatches(mainParam.filter, obj) &&
         checkPropertyTypeMatches(mainParam.filter, obj) &&
-        checkBathroomsMatches(mainParam.filter, obj)
+        checkBathroomsMatches(mainParam.filter, obj) &&
+        checkTenureMatches(mainParam.filter, obj) &&
+        checkPropertyConditionMatches(mainParam.filter, obj) &&
+        checkYearMatches(mainParam.filter, obj) &&
+        checkEnergyMatches(mainParam.filter, obj) &&
+        checkNearestMatches(mainParam.filter, obj) &&
+        checkFacilitiesMatches(mainParam.filter, obj)
       );
     })?.length;
     itemBedroomsCountList.push({
@@ -425,7 +541,13 @@ const getCountOnBathrooms = async (mainParam, estateObj) => {
         checkPriceMatches(mainParam.minPrice, mainParam.maxPrice, obj) &&
         checkSellerRatingMatches(mainParam.filter, obj) &&
         checkPropertyTypeMatches(mainParam.filter, obj) &&
-        checkBedroomsMatches(mainParam.filter, obj)
+        checkBedroomsMatches(mainParam.filter, obj) &&
+        checkTenureMatches(mainParam.filter, obj) &&
+        checkPropertyConditionMatches(mainParam.filter, obj) &&
+        checkYearMatches(mainParam.filter, obj) &&
+        checkEnergyMatches(mainParam.filter, obj) &&
+        checkNearestMatches(mainParam.filter, obj) &&
+        checkFacilitiesMatches(mainParam.filter, obj)
       );
     })?.length;
     itemBathroomsCountList.push({
@@ -434,6 +556,224 @@ const getCountOnBathrooms = async (mainParam, estateObj) => {
     });
   });
   return itemBathroomsCountList;
+};
+
+const getCountOnTenure = async (mainParam, estateObj) => {
+  let itemTenureCountList: any = [];
+  mainParam?.itemTenure.map((item: string, index: number) => {
+    let count = 0,
+      temp = "";
+    if (item != "Not Specified") {
+      temp = item;
+    }
+    count = estateObj.filter((obj) => {
+      const isMatchingTenure = (obj as any)?.itemDetailInfo?.tenure == temp;
+      const isMatchingItemCategory =
+        (obj as any).itemCategory == mainParam.itemCategory;
+
+      return (
+        isMatchingTenure &&
+        isMatchingItemCategory &&
+        checkPriceMatches(mainParam.minPrice, mainParam.maxPrice, obj) &&
+        checkSellerRatingMatches(mainParam.filter, obj) &&
+        checkPropertyTypeMatches(mainParam.filter, obj) &&
+        checkBedroomsMatches(mainParam.filter, obj) &&
+        checkBathroomsMatches(mainParam.filter, obj) &&
+        checkPropertyConditionMatches(mainParam.filter, obj) &&
+        checkYearMatches(mainParam.filter, obj) &&
+        checkEnergyMatches(mainParam.filter, obj) &&
+        checkNearestMatches(mainParam.filter, obj) &&
+        checkFacilitiesMatches(mainParam.filter, obj)
+      );
+    })?.length;
+    itemTenureCountList.push({
+      itemTenure: item,
+      count,
+    });
+  });
+  return itemTenureCountList;
+};
+
+const getCountOnPropertyCondition = async (mainParam, estateObj) => {
+  let itemPropertyConditionCountList: any = [];
+  mainParam?.itemPropertyCondition.map((item: string, index: number) => {
+    let count = 0,
+      temp = "";
+    if (item != "Not Specified") {
+      temp = item;
+    }
+    count = estateObj.filter((obj) => {
+      const isMatchingPropertyCondition =
+        (obj as any)?.itemDetailInfo?.condition == temp;
+      const isMatchingItemCategory =
+        (obj as any).itemCategory == mainParam.itemCategory;
+
+      return (
+        isMatchingPropertyCondition &&
+        isMatchingItemCategory &&
+        checkPriceMatches(mainParam.minPrice, mainParam.maxPrice, obj) &&
+        checkSellerRatingMatches(mainParam.filter, obj) &&
+        checkPropertyTypeMatches(mainParam.filter, obj) &&
+        checkBedroomsMatches(mainParam.filter, obj) &&
+        checkBathroomsMatches(mainParam.filter, obj) &&
+        checkTenureMatches(mainParam.filter, obj) &&
+        checkYearMatches(mainParam.filter, obj) &&
+        checkEnergyMatches(mainParam.filter, obj) &&
+        checkNearestMatches(mainParam.filter, obj) &&
+        checkFacilitiesMatches(mainParam.filter, obj)
+      );
+    })?.length;
+    itemPropertyConditionCountList.push({
+      itemPropertyCondition: item,
+      count,
+    });
+  });
+  return itemPropertyConditionCountList;
+};
+
+const getCountOnYear = async (mainParam, estateObj) => {
+  let itemYearCountList: any = [];
+  mainParam?.itemYear.map((item: string, index: number) => {
+    let count = 0,
+      temp = "";
+    if (item != "Not Specified") {
+      temp = item;
+    }
+    count = estateObj.filter((obj) => {
+      const isMatchingYear = (obj as any)?.itemDetailInfo?.year == temp;
+      const isMatchingItemCategory =
+        (obj as any).itemCategory == mainParam.itemCategory;
+
+      return (
+        isMatchingYear &&
+        isMatchingItemCategory &&
+        checkPriceMatches(mainParam.minPrice, mainParam.maxPrice, obj) &&
+        checkSellerRatingMatches(mainParam.filter, obj) &&
+        checkPropertyTypeMatches(mainParam.filter, obj) &&
+        checkBedroomsMatches(mainParam.filter, obj) &&
+        checkBathroomsMatches(mainParam.filter, obj) &&
+        checkTenureMatches(mainParam.filter, obj) &&
+        checkPropertyConditionMatches(mainParam.filter, obj) &&
+        checkEnergyMatches(mainParam.filter, obj) &&
+        checkNearestMatches(mainParam.filter, obj) &&
+        checkFacilitiesMatches(mainParam.filter, obj)
+      );
+    })?.length;
+    itemYearCountList.push({
+      itemYear: item,
+      count,
+    });
+  });
+  return itemYearCountList;
+};
+
+const getCountOnEnergy = async (mainParam, estateObj) => {
+  let itemEnergyCountList: any = [];
+  mainParam?.itemEnergy.map((item: string, index: number) => {
+    let count = 0,
+      temp = "";
+    if (item != "Not Specified") {
+      temp = item;
+    }
+    count = estateObj.filter((obj) => {
+      const isMatchingEnergy = (obj as any)?.itemDetailInfo?.energy == temp;
+      const isMatchingItemCategory =
+        (obj as any).itemCategory == mainParam.itemCategory;
+
+      return (
+        isMatchingEnergy &&
+        isMatchingItemCategory &&
+        checkPriceMatches(mainParam.minPrice, mainParam.maxPrice, obj) &&
+        checkSellerRatingMatches(mainParam.filter, obj) &&
+        checkPropertyTypeMatches(mainParam.filter, obj) &&
+        checkBedroomsMatches(mainParam.filter, obj) &&
+        checkBathroomsMatches(mainParam.filter, obj) &&
+        checkTenureMatches(mainParam.filter, obj) &&
+        checkPropertyConditionMatches(mainParam.filter, obj) &&
+        checkYearMatches(mainParam.filter, obj) &&
+        checkNearestMatches(mainParam.filter, obj) &&
+        checkFacilitiesMatches(mainParam.filter, obj)
+      );
+    })?.length;
+    itemEnergyCountList.push({
+      itemEnergy: item,
+      count,
+    });
+  });
+  return itemEnergyCountList;
+};
+
+const getCountOnNearest = async (mainParam, estateObj) => {
+  let itemNearestCountList: any = [];
+  mainParam?.itemNearest.map((item: string, index: number) => {
+    let count = 0,
+      temp = "";
+    if (item != "Not Specified") {
+      temp = item;
+    }
+    count = estateObj.filter((obj) => {
+      const isMatchingNearest = (obj as any)?.itemDetailInfo?.nearest == temp;
+      const isMatchingItemCategory =
+        (obj as any).itemCategory == mainParam.itemCategory;
+
+      return (
+        isMatchingNearest &&
+        isMatchingItemCategory &&
+        checkPriceMatches(mainParam.minPrice, mainParam.maxPrice, obj) &&
+        checkSellerRatingMatches(mainParam.filter, obj) &&
+        checkPropertyTypeMatches(mainParam.filter, obj) &&
+        checkBedroomsMatches(mainParam.filter, obj) &&
+        checkBathroomsMatches(mainParam.filter, obj) &&
+        checkTenureMatches(mainParam.filter, obj) &&
+        checkPropertyConditionMatches(mainParam.filter, obj) &&
+        checkYearMatches(mainParam.filter, obj) &&
+        checkEnergyMatches(mainParam.filter, obj) &&
+        checkFacilitiesMatches(mainParam.filter, obj)
+      );
+    })?.length;
+    itemNearestCountList.push({
+      itemNearest: item,
+      count,
+    });
+  });
+  return itemNearestCountList;
+};
+
+const getCountOnFacilities = async (mainParam, estateObj) => {
+  let itemFacilitiesCountList: any = [];
+  mainParam?.itemFacilities.map((item: string, index: number) => {
+    let count = 0,
+      temp = "";
+    if (item != "Not Specified") {
+      temp = item;
+    }
+    count = estateObj.filter((obj) => {
+      const isMatchingFacilities =
+        (obj as any)?.itemDetailInfo?.facilities == temp;
+      const isMatchingItemCategory =
+        (obj as any).itemCategory == mainParam.itemCategory;
+
+      return (
+        isMatchingFacilities &&
+        isMatchingItemCategory &&
+        checkPriceMatches(mainParam.minPrice, mainParam.maxPrice, obj) &&
+        checkSellerRatingMatches(mainParam.filter, obj) &&
+        checkPropertyTypeMatches(mainParam.filter, obj) &&
+        checkBedroomsMatches(mainParam.filter, obj) &&
+        checkBathroomsMatches(mainParam.filter, obj) &&
+        checkTenureMatches(mainParam.filter, obj) &&
+        checkPropertyConditionMatches(mainParam.filter, obj) &&
+        checkYearMatches(mainParam.filter, obj) &&
+        checkEnergyMatches(mainParam.filter, obj) &&
+        checkNearestMatches(mainParam.filter, obj)
+      );
+    })?.length;
+    itemFacilitiesCountList.push({
+      itemFacilities: item,
+      count,
+    });
+  });
+  return itemFacilitiesCountList;
 };
 
 export const checkPropertyTypeMatches = (filter, obj) => {
@@ -458,4 +798,76 @@ export const checkBathroomsMatches = (filter, obj) => {
     ? filter.bathrooms.includes((obj as any)?.itemDetailInfo?.bathrooms)
     : true;
   return bathroomsMatches;
+};
+
+export const checkTenureMatches = (filter, obj) => {
+  const selectedTenureCondition = filter.tenure?.length > 0;
+  let index = filter.tenure.indexOf("Not Specified");
+  if (index > -1) {
+    filter.tenure[index] = "";
+  }
+  const tenureMatches = selectedTenureCondition
+    ? filter.tenure.includes((obj as any)?.itemDetailInfo?.tenure)
+    : true;
+  return tenureMatches;
+};
+
+export const checkPropertyConditionMatches = (filter, obj) => {
+  const selectedPropertyConditionCondition = filter.condition?.length > 0;
+  let index = filter.condition.indexOf("Not Specified");
+  if (index > -1) {
+    filter.condition[index] = "";
+  }
+  const propertyConditionMatches = selectedPropertyConditionCondition
+    ? filter.condition.includes((obj as any)?.itemDetailInfo?.condition)
+    : true;
+  return propertyConditionMatches;
+};
+
+export const checkYearMatches = (filter, obj) => {
+  const selectedYearCondition = filter.year?.length > 0;
+  let index = filter.year.indexOf("Not Specified");
+  if (index > -1) {
+    filter.year[index] = "";
+  }
+  const yearMatches = selectedYearCondition
+    ? filter.year.includes((obj as any)?.itemDetailInfo?.year)
+    : true;
+  return yearMatches;
+};
+
+export const checkEnergyMatches = (filter, obj) => {
+  const selectedEnergyCondition = filter.energy?.length > 0;
+  let index = filter.energy.indexOf("Not Specified");
+  if (index > -1) {
+    filter.energy[index] = "";
+  }
+  const energyMatches = selectedEnergyCondition
+    ? filter.energy.includes((obj as any)?.itemDetailInfo?.energy)
+    : true;
+  return energyMatches;
+};
+
+export const checkNearestMatches = (filter, obj) => {
+  const selectedNearestCondition = filter.nearest?.length > 0;
+  let index = filter.nearest.indexOf("Not Specified");
+  if (index > -1) {
+    filter.nearest[index] = "";
+  }
+  const nearestMatches = selectedNearestCondition
+    ? filter.nearest.includes((obj as any)?.itemDetailInfo?.nearest)
+    : true;
+  return nearestMatches;
+};
+
+export const checkFacilitiesMatches = (filter, obj) => {
+  const selectedFacilitiesCondition = filter.facilities?.length > 0;
+  let index = filter.facilities.indexOf("Not Specified");
+  if (index > -1) {
+    filter.facilities[index] = "";
+  }
+  const facilitiesMatches = selectedFacilitiesCondition
+    ? filter.facilities.includes((obj as any)?.itemDetailInfo?.facilities)
+    : true;
+  return facilitiesMatches;
 };
